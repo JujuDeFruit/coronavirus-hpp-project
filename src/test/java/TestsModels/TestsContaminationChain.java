4b7be738c091257f92e21b93353d7b7158f51146
JujuDeFruit <julien.raynal@yahoo.fr>
2021-05-05 12:21:20 +0200

    Contamination Type avancement

--- src/test/java/TestsModels/TestsContaminationChain.java
+++ src/test/java/TestsModels/TestsContaminationChain.java
@@ -13,9 +13,16 @@ public class TestsContaminationChain {
         DataType first = new DataType("4", "Daniel", "ROBINSON", "1582161158.5235808", "unknown", (short)0);
         ContaminationChain contaminationChain = new ContaminationChain(first);
 
+        assertEquals(10, contaminationChain.getScore());
+
         DataType second = new DataType("5", "Jessica", "WATSON", "1583091884.9200459", "4", (short)0);
         contaminationChain.push(second);
 
-        assertEquals(20, contaminationChain.getScore());
+        assertEquals(14, contaminationChain.getScore());
+
+        DataType third = new DataType("9", "Stephanie", "MITCHELL", "1585699579.2617905", "2", (short)0);
+        contaminationChain.push(third);
+
+        assertEquals(14, contaminationChain.getScore());
     }
 }
