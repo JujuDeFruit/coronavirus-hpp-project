78ecab1448effbbd595c30943d861fc52631436d
JujuDeFruit <julien.raynal@yahoo.fr>
2021-05-05 09:57:24 +0200

    Changement data type constructor

--- src/test/java/TestsModels/TestsDataType.java
+++ src/test/java/TestsModels/TestsDataType.java
@@ -12,14 +12,14 @@ public class TestsDataType {
 
     @Test
     public void test (){
-        DataType data = new DataType("4, \"Daniel\", \"ROBINSON\", 1995-08-21 00:00:00, 1582161158.5235808, unknown, \"course à pieds avec la grand-mère au marché\"");
+        DataType data = new DataType("4", "\"Daniel\"", "\"ROBINSON\"", "1582161158.5235808", "unknown");
 
         assertEquals(4, data.getPerson_id());
         assertEquals("Daniel ROBINSON", data.getPerson());
         assertEquals(new Timestamp((long)1582161158.5235808), data.getDiagnosed_ts());
         assertEquals(-1, data.getContaminated_by());
 
-        DataType data2 = new DataType("9, \"Stephanie\", \"MITCHELL\", 1924-03-17 00:00:00, 1585699579.2617905, 4, \"promenade avec mon fils à la campagne\"");
+        DataType data2 = new DataType("9", "\"Stephanie\"", "\"MITCHELL\"", "1585699579.2617905", "4");
 
         assertEquals(9, data2.getPerson_id());
         assertEquals("Stephanie MITCHELL", data2.getPerson());
