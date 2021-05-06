cd006483a617116c9640633fc2207112cd23ef63
DEGRANGE <alexis-deg@hotmail.fr>
2021-05-06 12:05:32 +0200

    commit before pull

--- src/main/java/Runners/Processing.java
+++ src/main/java/Runners/Processing.java
@@ -8,12 +8,22 @@ import java.util.ListIterator;
 import java.util.Vector;
 import java.util.concurrent.BlockingQueue;
 
-import javax.print.attribute.Size2DSyntax;
 
 import Models.ContaminationChain;
 import Models.DataType;
 
-
+/**
+ * Process who creates chain of contamination, sort them, and add them in a Queue 
+ *
+ * <ul>
+ *     <li>inQueue_ : BlockingQueue of Datatype, Datatypre represent a person</li>
+ *     <li>outQueue_: BlockingQueue of ContaminationChain[], represent the 3 contaminatedChain with the highest score</li>
+ *     <li>VectorOfContaminationChain_ : Vector of ContaminatedChain</li>
+ *     <li>ending : boolean</li>
+ *     <li>currentTimestamp : </li>
+ *     <li>poisonPill : poison-pill for inQueue_</li>
+ * </ul>
+ */
 public class Processing implements Runnable {
 	private final BlockingQueue<DataType> inQueue_;
 	private final BlockingQueue<ContaminationChain[]> outQueue_;
@@ -29,16 +39,22 @@ public class Processing implements Runnable {
 		VectorOfContaminationChain_=VectorOfContaminationChain;
 	}
 	
+   /**
+    * Run the thread  
+    */
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
 		try {
 			DataType onePerson = inQueue_.take();
+			//while we don't reach the poison pill "-1" do:
 			while(onePerson.getPerson_id()!=-1) {
 				processId(onePerson);
+				//
 				int size = VectorOfContaminationChain_.size();
 				ContaminationChain[] top3 = {VectorOfContaminationChain_.get(size - 1), VectorOfContaminationChain_.get(size - 2), VectorOfContaminationChain_.get(size - 3)};
 				outQueue_.add(top3);
+				//we take a other person
 				onePerson = inQueue_.take();
 			}
 			// poison-pill
@@ -49,6 +65,10 @@ public class Processing implements Runnable {
 		}
 	}
 		
+	/**
+	 * Process who ..
+	 * @param myPerson : the person taken in charge in the inQueue_
+	 */
 	private void processId(DataType myPerson){
 		currentTimestamp = myPerson.getDiagnosed_ts();
 		VectorOfContaminationChain_.forEach((myContaminationChain)->{
@@ -64,16 +84,16 @@ public class Processing implements Runnable {
 				List<Integer> listOfIdContamination = myContaminationChain.getContaminationId();
 				ListIterator<Integer> iterator = listOfIdContamination.listIterator(listOfIdContamination.size());
 				int contaminatedBy = myPerson.getContaminated_by();
-				while(iterator.hasPrevious() && !ending) {
+				while(iterator.hasPrevious() && ending==false) {
 					ending=iterator.equals(contaminatedBy);
 					iterator.previous();
 				}
-				if(ending) {
+				if(ending==true) {
 					myContaminationChain.push(myPerson);
 				}	
 			});
 			//if ending=false mean that the person was contaminated by a chain with a score of 0 so she has been destroyed 
-			if(!ending) {
+			if(ending==false) {
 				VectorOfContaminationChain_.add(new ContaminationChain(myPerson));						
 			}
 		}
@@ -83,6 +103,9 @@ public class Processing implements Runnable {
 		outQueue_.add(top3);
 	}
 	
+	/**
+	 * Process who sort the Vector of contaminationChain by their score
+	 */
 	private void processSort() {
 		Collections.sort(VectorOfContaminationChain_, Comparator.comparing(ContaminationChain::getScore));
 	}
